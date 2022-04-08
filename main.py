57d1eab931297d2910e3ff163563010f78b42fc0
jlanglois4 <jlanglois4@my.wctc.edu>
2022-04-07 20:11:56 -0500

    Mod 2 Lab A

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 intrusions = int(input("Please enter the number of intrusions: "))
 days = int(input("Please enter the number of days tracked: "))
-average_intrusions_per_day = intrusions / days
-print("Average intrusions per day:", average_intrusions_per_day.__round__(2))
+avg_intrusions_per_day = (intrusions / days)
+print(f"Average intrusions per day: {avg_intrusions_per_day:,.2f}")
+
+low_activity_upper_range = 20
+average_activity_upper_range = 100
+high_activity_upper_range = 200
+
+low_activity = "Low"
+average_activity = "Average"
+high_activity = "High"
+critical_activity = "Critical"
+
+activity_level_output = f"Activity level:"
+if avg_intrusions_per_day > high_activity_upper_range:
+    print(activity_level_output, critical_activity)
+elif avg_intrusions_per_day > average_activity_upper_range:
+    print(activity_level_output, high_activity)
+elif avg_intrusions_per_day > low_activity_upper_range:
+    print(activity_level_output, average_activity)
+else:
+    print(activity_level_output, low_activity)
