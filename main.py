9dd6b36ac1a34ef287745db8a6a207606d5c4a3f
jlanglois4 <jlanglois4@my.wctc.edu>
2022-04-15 13:37:48 -0500

    Implemented activity level information into a list. Activity level list and condition code moved into its own method named range_calculator. Condition code changed from nested if else's to if statement in a for loop.

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 intrusions = int(input("Please enter the number of intrusions: "))
 days = int(input("Please enter the number of days tracked: "))
-avg_intrusions_per_day = (intrusions / days)
+avg_intrusions_per_day = intrusions / days
 print(f"Average intrusions per day: {avg_intrusions_per_day:,.2f}")
 
-low_activity_upper_range = 20
-average_activity_upper_range = 100
-high_activity_upper_range = 200
 
-low_activity = "Low"
-average_activity = "Average"
-high_activity = "High"
-critical_activity = "Critical"
-activity_level = ""
+def range_calculator(avg_intrusions):
+    # lower_range, activity_level
 
-if avg_intrusions_per_day >= high_activity_upper_range:
-    activity_level = critical_activity
-elif avg_intrusions_per_day >= average_activity_upper_range:
-    activity_level = high_activity
-elif avg_intrusions_per_day >= low_activity_upper_range:
-    activity_level = average_activity
-else:
-    activity_level = low_activity
+    activity_level = ""
+    activity_list = [
+        [200, "Critical"],
+        [100, "High"],
+        [20, "Average"],
+        [0, "Low"]
+    ]
 
-print(f"Activity level: {activity_level}")
+    for item in activity_list:
+        if avg_intrusions >= item[0]:
+            activity_level = item[1]
+            break
+
+    print(f"Activity level: {activity_level}")
+
+
+range_calculator(avg_intrusions_per_day)
